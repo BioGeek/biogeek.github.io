82a530fddde9d649b48775dbaf5be1699873cc05
Jeroen Van Goey <j.vangoey@instadeep.com>
2022-09-02 15:14:34 +0200

    Updates

---
+++ component---src-pages-404-jsx-cfc10611447b456eb33a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdevfolio=self.webpackChunkdevfolio||[]).push([[256],{7291:function(e,t,n){n.r(t);var l=n(7294),o=n(1597),u=n(1988),a=n(8633),r="text-lg font-bold",s="underline";t.default=function(){return l.createElement(u.Z,null,l.createElement(a.Z,{title:"Not found"}),l.createElement("h1",{className:r},"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist."," ",l.createElement(o.Link,{className:s,to:"/"},"Return to safety"),"."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-cfc10611447b456eb33a.js.map
